91843bbd62315e7dddf195e36bacbebee148a259
Nicolas DOBROWOLSKI <nicolas.dobrowolski@imt-lille-douai.fr>
2021-04-20 12:39:29 +0200

    Scraping app

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+// 1- Change URL
+// 2- Change cookie session
+// 3- Run server PHP
+// 4- Check find()
+// > php -S 127.0.0.1:8000
+
+require_once './simple_html_dom.php';
+$url = "https://siteweb.com";
+$ch = curl_init($url); 
+//curl_setopt($ch, CURLOPT_VERBOSE, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_COOKIE, 'name=value'); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+$response = curl_exec($ch);
+curl_close($ch);
+//----
+$html = new simple_html_dom();
+$html->load($response);
+$accordion = $html->find('div.panel-group');
+$items = array();
+$index = 0;
+foreach($html->find('div.panel-group') as $html) {
+    foreach($html->find('div.panel.panel-default') as $panel){
+        $items[$index++] = $panel->plaintext;
+    }
+}
+var_dump($items);
+
